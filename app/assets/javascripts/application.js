194bade491743a3e11a31da6db9a1efecc038ed6
Gabriel Albo <gabriel@albo.com.br>
2012-12-26 16:38:37 -0200

    added click and hover events in a grotesque manner. Need to come back here and call references instead of JQuery's hover and click for every single element all over again.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,119 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+
+    	$("#fo1").hover(function(){
+    		$("#fo1").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#fo1").css("text-shadow", "");
+    	});			
+
+    	$("#fo2").hover(function(){
+    		$("#fo2").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#fo2").css("text-shadow", "");
+    	});			
+
+		$("#rm1").hover(function(){
+    		$("#rm1").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#rm1").css("text-shadow", "");
+    	});			
+
+    	$("#rm2").hover(function(){
+    		$("#rm2").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#rm2").css("text-shadow", "");
+    	});		
+
+    	$("#is1").hover(function(){
+    		$("#is1").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#is1").css("text-shadow", "");
+    	});		
+
+    	$("#is2").hover(function(){
+    		$("#is2").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#is2").css("text-shadow", "");
+    	});		
+
+    	$("#empti1").hover(function(){
+    		$("#empti1").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#empti1").css("text-shadow", "");
+    	});		
+
+    	$("#empti2").hover(function(){
+    		$("#empti2").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#empti2").css("text-shadow", "");
+    	});
+
+    	$("#ness1").hover(function(){
+    		$("#ness1").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#ness1").css("text-shadow", "");
+    	});		
+
+    	$("#ness2").hover(function(){
+    		$("#ness2").css("text-shadow", "1px 1px grey");
+    	},
+    	function(){
+    		$("#ness2").css("text-shadow", "");
+    	});		
+
+
+
+    	$("#fo1").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull01");
+    	});
+    	$("#rm1").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull02");
+    	});
+    	$("#is1").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull03");
+    	});
+    	$("#empti1").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull04");
+    	});
+    	$("#ness1").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull05");
+    	});
+    	$("#empti2").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull06");
+    	});
+    	$("#ness2").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull07");
+    	});
+    	$("#is2").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull08");
+    	});
+    	$("#fo2").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull09");
+    	});
+    	$("#rm2").click(function(){
+    		$(".bulls").removeClass().addClass("bulls");
+    		$(".bulls").addClass("bull10");
+    	});
+    });
